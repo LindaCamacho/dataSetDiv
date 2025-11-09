6cedd21d0bae5306e2f02ddd9232a5cb2a73e4f8
LindaCamacho <ceo130506@gmail.com>
2025-11-09 13:01:46 -0600

    Limpieza de entorno virtual y git entero...

---
+++ dataapp/uploader/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.upload_dataset, name="upload"),
+    path("split/", views.split_dataset, name="split"),
+    path("download/pdf/", views.download_pdf, name="download_pdf"),
+    path("download/excel/", views.download_excel, name="download_excel"),
+]
